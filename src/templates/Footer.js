6af0e28edd8d6b7eb775259aeae179b52f001a79
IndiraPe <forever_tk_16@hotmail.com>
2023-04-05 19:15:18 -0500

    ADD: up changes in responsive desktop

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -1,9 +1,11 @@
 const Footer = () => {
   const viewFooter = `
-    
-      <a href="https://github.com/LadyDi3103" target="_blank">Diana Quispe</a>, 
-      <a href="https://github.com/ifdotcom" target="_blank">Fernanda Vidal</a> 
-      e <a href="https://github.com/IndiraPe" target="_blank">Indira Pérez </a> ©Derechos Reservados
+    <img src="./img/github.png" alt="Logo de Github"/>
+    <a href="https://github.com/LadyDi3103" target="_blank">Diana Quispe</a> & 
+    <img src="./img/github.png" alt="Logo de Github"/>
+    <a href="https://github.com/ifdotcom" target="_blank">Fernanda Vidal</a> 
+    & <img src="./img/github.png" alt="Logo de Github"/>
+    <a href="https://github.com/IndiraPe" target="_blank">Indira Pérez </a>  ©Derechos Reservados
   
     `;
   return viewFooter;
