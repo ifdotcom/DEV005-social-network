7ee90728ff483938185184eec5dc88df5c529079
LadyDi3103 <chinamy2004@gmail.com>
2023-05-02 22:56:52 -0500

    UPDATE:TESTS

--- test/pages/Dashboard.spec.js
+++ test/pages/Dashboard.spec.js
@@ -3,6 +3,8 @@
  */
 import * as FIREBASE from 'firebase/auth';
 import Dashboard from '../../src/pages/Dashboard.js';
+// import * as emailCutter from '../../src/pages/Dashboard.js';
+// import { savePostFire } from '../../src/lib/Posts.js';
 
 jest.mock('firebase/auth');
 
@@ -37,5 +39,28 @@ describe('Dashboard', () => {
     const DOM = document.createElement('div');
     DOM.append(Dashboard());
     expect(DOM).toMatchSnapshot();
+  });
+  test('After click "NO" Modal display change to none', () => {
+    const DOM = document.createElement('div');
+    DOM.append(Dashboard());
+    const not = DOM.querySelector('#not');
+    const myModal = DOM.querySelector('#myModal');
+    not.click();
+    expect(myModal.style.display).toBe('none');
+  });
+  /* test.skip('Should cut the user email to be shown as the username', () => {
+    emailCutter;
+  }); */
+  test('button de pub..', (done) => {
+    const DOM = document.createElement('div');
+    DOM.append(Dashboard());
+    const savePostFire = jest.fn();
+    const btnPost = DOM.querySelector('#button-post');
+    const postText = DOM.querySelector('#post-text');
+    // const editStatus = true;
+    btnPost.click();
+
+    expect(savePostFire).toHaveBeenCalledWith(postText);
+    done();
   });
 });
